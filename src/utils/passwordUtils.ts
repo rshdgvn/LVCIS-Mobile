ebcd9b6d01c66c888400a2d56e33afa8403f98fc
rshdgvn <rasheedgavinesponga@gmail.com>
2026-03-22 20:33:37 +0800

    fix: update password special character validation regex

--- src/utils/passwordUtils.ts
+++ src/utils/passwordUtils.ts
@@ -1,7 +1,7 @@
 export const evaluatePasswordStrength = (password: string) => {
   const hasMinLength = password.length >= 8;
   const hasNumber = /\d/.test(password);
-  const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const hasSpecial = /[^A-Za-z0-9]/.test(password);
 
   const strength = [hasMinLength, hasNumber, hasSpecial].filter(Boolean).length;
 
